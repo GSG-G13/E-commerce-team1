d28b4694641198cc0f98dfe2652198de66e0ad97
EmanAlabsi <emanabsi30@gmail.com>
2023-03-08 09:42:52 +0200

    edit files

---
+++ js/customer.js
@@ -0,0 +1,106 @@
+const products = [
+  {
+    name: "MAC",
+    category: "Laptops",
+    price: "30",
+    image: "white-tshirt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,amet!",
+  },
+  {
+    name: "HP",
+    category: "Laptops",
+    price: "30",
+    image: "white-tshirt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,amet!",
+  },
+  {
+    name: "iPhonex",
+    category: "Mobiles",
+    price: "30",
+    image: "white-tshirt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,amet!",
+  },
+  {
+    name: "Nokia",
+    category: "Mobiles",
+    price: "30",
+    image: "white-tshirt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,amet!",
+  },
+  {
+    name: "Wireless-hedphones",
+    category: "Headphones",
+    price: "30",
+    image: "white-tshirt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,amet!",
+  },
+  {
+    name: "headphone",
+    category: "Headphones",
+    price: "30",
+    image: "white-tshirt.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,amet!",
+  },
+];
+
+let filteredProducts = [...products];
+
+//get elemnts from the dom
+const productsContainer = document.querySelector(".products");
+const searchBar = document.querySelector(".input-search");
+const productContainer = document.querySelector(".product-div");
+const list = document.getElementById("list");
+
+list.addEventListener("change", (e) => {
+  filteredProducts =
+    list.value === "all"
+      ? [...products]
+      : products.filter((p) => p.category === list.value);
+
+  console.log(list.value, filteredProducts);
+
+  updateDom();
+});
+
+searchBar.addEventListener("input", (e) => {
+  filteredProducts = e.target.value
+    ? filteredProducts.filter((p) =>
+        p.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    : [...products];
+
+  updateDom();
+});
+
+function createProductElement(product) {
+  return `
+  <div class="product-div">
+  <div class="product-details">
+    <img src="${product.image}" alt="" class="product-img" />
+    <div class="product-name-price">
+      <p class="product-name">${product.name}</p>
+      <p class="product-price">${product.price}$</p>
+    </div>
+    <p class="product-category">${product.category}</p>
+    <p class="product-description">
+      ${product.description}
+    </p>
+    <button class="add-to-cart">Add to Cart</button>
+  </div>
+</div>
+  `;
+}
+
+function updateDom() {
+  const plainElements = filteredProducts.map((p) => createProductElement(p));
+
+  productsContainer.innerHTML = plainElements;
+}
+
+updateDom();
